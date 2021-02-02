9dd750ddc83ad1c2d977f6a85bb1e801e96ba5a6
AdityaBhattacharya1 <utoprocess@gmail.com>
2021-02-02 12:22:18 +0530

    Added DRY functionalities
    Added try-catches for better functionality, added functions which make the job easier

--- main.py
+++ main.py
@@ -4,29 +4,56 @@ import mplfinance as mpf
 import datetime as dt
 from datetime import timedelta
 
-end_date = dt.datetime.now()
+
+def date_setter(str_value, phonetic):
+    return int(input(f'Enter the {str_value} {phonetic} which the stock value should be counted \n'))
+
+
+# Setting empty values of the start and end date or else it throws an error
+start_date = None
+end_date = None
+
+
+def date_validator():
+    global start_date
+    global end_date
+    try:
+        start_year = date_setter('year', 'from')
+        start_month = date_setter('month', 'from')
+        start_day = date_setter('date of day', 'from')
+
+        end_year = date_setter('year', 'till')
+        end_month = date_setter('month', 'till')
+        end_day = date_setter('date of day', 'till')
+    except:
+        print('Since numeric value was not entered for either the date, month or year, defaulted to that of 31 days ago')
+        start_date = dt.datetime.today() - timedelta(days=31)
+        end_date = dt.datetime.now()
+    else:
+        start_date = dt.datetime(start_year, start_month, start_day)
+        end_date = dt.datetime(end_year, end_month, end_day)
+
+    if start_year < 1900 or end_date < start_date:
+        print(
+            "Invalid year value. Set to default value(s) [Defaults: start date is that of 31 days ago, end date is today's date]")
+        start_date = dt.datetime.today() - timedelta(days=31)
+        end_date = dt.datetime.now()
+
+    return start_date, end_date
+
 
 graph_types = ['candle', 'line', 'ohlc', 'renko', 'pnf']
 
 stock_name = input('Enter the name of the stock\n')
 graph_type = input(
     'Enter type of graph\n [Options available: candle, line, ohlc, renko, pnf]\n')
+mav_val = int(input(
+    'Enter the moving average value\n'))
 
 if not graph_type in graph_types:
     print('Invalid graph type, defaulted to candlestick')
 
-try:
-    start_year = int(
-        input('Enter the year from which the value should be counted \n'))
-    start_month = int(
-        input('Enter the month from which the value should be counted \n'))
-    start_day = int(
-        input('Enter the date of the day from which the value should be counted \n'))
-except:
-    print('Since numeric value was not entered for either the date, month or year, defaulted to that of 31 days ago')
-    start_date = dt.datetime.today() - timedelta(days=31)
-else:
-    start_date = dt.datetime(start_year, start_month, start_day)
+date_validator()
 
 
 # Uses the Yahoo stocks API for remote data collection.
@@ -49,11 +76,13 @@ mpf_style = mpf.make_mpf_style(
 
 # The double new-line escapes because the title would stick to the option bar.
 # The new lines act as psuedo-styles as there is no option available for styling the titles (yet).
-# What is this 'mav', you ask? Here's an ELI5 link: https://www.reddit.com/r/explainlikeimfive/comments/a7pbys/eli5_what_is_a_moving_average_how_is_it_different/
-# tl;dr it is the average of the most recent values. The value of the mav determines how many days are counted in the average.
+# What is this 'mav', you ask? Here's an ELI5 link:
+# https://www.reddit.com/r/explainlikeimfive/comments/a7pbys/eli5_what_is_a_moving_average_how_is_it_different/
+# tl;dr it is the average of the most recent values.
+# The value of the mav determines how many days are counted in the average.
 # The show_nontrading boolean includes the non-trading days (holidays, etc.) in the graph.
 
 
-mpf.plot(data, title=f"\n\n {stock_name} stock value",
+mpf.plot(data, title=f"\n\n {stock_name} stock value\n",
          type=f"{graph_type if graph_type in graph_types else 'candlestick'}",
-         style=mpf_style, volume=True, mav=2, show_nontrading=False)
+         style=mpf_style, volume=True, mav=mav_val, show_nontrading=False)
